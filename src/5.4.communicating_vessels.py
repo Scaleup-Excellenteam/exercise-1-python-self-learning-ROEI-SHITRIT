ec5b01f5dcbf09da1f24f0869881dd7aed5cb60d
ROEI-SHITRIT <roeishi489@gmail.com>
2025-04-01 19:15:08 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -32,18 +32,18 @@ def generator_interleave(*iterable):
 
 
 def interleave(*iterable):
-     """
-    Merges multiple iterables by interleaving their elements, skipping None values.
+    """
+     Merges multiple iterables by interleaving their elements, skipping None values.
 
-    Args:
-        *iterable: One or more iterables to be interleaved.
+     Args:
+         *iterable: One or more iterables to be interleaved.
 
-    Returns:
-        list: A list containing elements from the input iterables in an interleaved order.
+     Returns:
+         list: A list containing elements from the input iterables in an interleaved order.
 
-    Example:
-        #>>> interleave('abc', [1, 2, 3], ('!', '@', '#'))
-        ['a', 1, '!', 'b', 2, '@', 'c', 3, '#']
+     Example:
+         #>>> interleave('abc', [1, 2, 3], ('!', '@', '#'))
+         ['a', 1, '!', 'b', 2, '@', 'c', 3, '#']
     """
     return [item for it in zip_longest(*iterable) for item in it if item is not None]
 
