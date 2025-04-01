a9c9c85d91ca4981d245fbf947722fd87033314f
ROEI-SHITRIT <roeishi489@gmail.com>
2025-04-01 19:39:37 +0300

    Update 6.4.remember_remember.py

--- src/6.4.remember_remember.py
+++ src/6.4.remember_remember.py
@@ -6,7 +6,8 @@ def remember_remember(path):
        Decodes a secret message embedded in a black-and-white image by locating black pixels in each column.
 
        The function opens an image from the given file path, iterates through each column, and checks for the
-       row index of the black pixel (value 1). The row indices are then converted to characters (using the `chr` function)
+       row index of the black pixel (value 1).
+       The row indices are then converted to characters (using the `chr` function)
        to reveal the hidden message.
 
        Args:
@@ -19,14 +20,14 @@ def remember_remember(path):
            #>>> remember_remember("encrypted_message.png")
            "Hello"
        """
-  
+
     result = ""
 
     try:
         # Open the image and convert it to black and white (L mode)
         image = Image.open(path).convert('1')  # '1' mode is for 1-bit pixels (black and white)
     except FileNotFoundError:
-        exit(-1)
+        print("file not found")
 
     # Get the width and height of the image
     width, height = image.size
@@ -35,10 +36,10 @@ def remember_remember(path):
     for col in range(width):
         for row in range(height):
             pixel = image.getpixel((col, row))
-            if pixel == 0: 
+            if pixel == 0:
                 result += chr(row)  
 
     return result
 
-if __name__ == '__main__':
+  if __name__ == '__main__':
   print(remember_remember("code.png"))
