48c7812cef8192c089f141cd139ed021b59aa799
ROEI-SHITRIT <roeishi489@gmail.com>
2025-04-01 17:46:18 +0300

    Update 5.2.piece_of_cake.py

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -1,45 +1,46 @@
+"""    Calculates the total price of ingredients for a recipe based on their quantities and prices."""
 from argparse import ArgumentError
 
 
-def piece_of_cake(prices,optionals=None,**quantities):
+def piece_of_cake(prices, optionals=None, **quantities):
     """
-      Calculates the total price of ingredients for a recipe based on their quantities and prices.
 
-      Args:
-          prices (dict): A dictionary with ingredient names as keys and their price per 100 grams as values.
-          optionals (list, optional): A list of ingredients to exclude from the calculation. Defaults to None.
-          **quantities (dict): The quantities of each ingredient in grams.
+    Args:
+        prices (dict): A dictionary with ingredient names as keys and their price per 100 grams as values.
+        optionals (list, optional): A list of ingredients to exclude from the calculation. Defaults to None.
+        **quantities (dict): The quantities of each ingredient in grams.
 
-      Returns:
-          float: The total price for the ingredients used in the recipe.
+    Returns:
+        float: The total price for the ingredients used in the recipe.
 
-      Example:
-          #>>> get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
-          44.0
+    Example:
+        #>>> get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
+        44.0
 
-          #>>> get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
-          54.0
-      """
-    sum=0
+        #>>> get_recipe_price({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
+        54.0
+    """
+    sum__to_send = 0
     try:
-     if len(prices)==0:
-         return 0
-     for key,value in prices.items():
-         if optionals is not None and key in optionals:
-             continue
+        if len(prices) == 0:
+            return 0
+        for key, value in prices.items():
+            if optionals is not None and key in optionals:
+                continue
 
-         if key in quantities:
-          sum+=(quantities[key]/100)*value
+            if key in quantities:
+                sum__to_send += (quantities[key] / 100) * value
 
-     return sum
+        return sum__to_send
 
     except TypeError:
         print('TypeError')
-        return
+        return None
     except ArgumentError:
         print('ArgumentError')
-        return
+        return None
+
 
 if __name__ == '__main__':
-   piece_of_cake({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
-   piece_of_cake({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
+    piece_of_cake({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100)
+    piece_of_cake({'chocolate': 18, 'milk': 8}, optionals=['milk'], chocolate=300)
