b90bf7e1dcfbae12bf5c9b8fa9994a5109f3fc64
ROEI-SHITRIT <roeishi489@gmail.com>
2025-04-01 17:59:45 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -1,3 +1,4 @@
+"""Interleaves multiple iterables, either as a generator or returning a list."""
 from itertools import zip_longest
 
 
@@ -14,26 +15,25 @@ def generator_interleave(*iterable):
        Example:
           # >>> list(interleave('abc', [1, 2, 3], ('!', '@', '#')))
            ['a', 1, '!', 'b', 2, '@', 'c', 3, '#']
-       """
-    i=0
-    lst=list(iterable)
+    """
+    i = 0
+    lst_to_send = list(iterable)
     try:
-     while len(lst)>0:
-        for item in lst:
-            if i < len(item):
-                yield item[i]
-            else:
-                lst.remove(item)
-        i+=1
+        while len(lst) > 0:
+            for item in lst:
+                if i < len(item):
+                    yield item[i]
+                else:
+                    lst_to_send.remove(item)
+            i += 1
     except ValueError:
-        exit(-1)
+        return None
 
 
 def interleave(*iterable):
-
     return [item for it in zip_longest(*iterable) for item in it if item is not None]
 
 
 if __name__ == '__main__':
-  print(list(generator_interleave('abc', [1, 2, 3], ('!', '@', '#','*'))))
-  print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    print(list(generator_interleave('abc', [1, 2, 3], ('!', '@', '#', '*'))))
+    print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
